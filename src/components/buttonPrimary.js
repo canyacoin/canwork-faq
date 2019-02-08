65f89f7881e974842d18542b24417f37b6cff75f
wie <wie.cbd@gmail.com>
2019-02-08 14:25:08 +1100

    completely migrated to gitbook

--- src/components/buttonPrimary.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-
-export default ({ data }) => {
-    <button className={data.classes}>
-        {data.btnText}
-    </button>
-}
--- src/readme.md
+++
@@ -1,4 +0,0 @@
-**Image Repo
-
-
-![Image](https://gitlab.com/wie-cbd/canwork-blog/raw/master/src/Images/2018-10-05_16.01.09.jpg)
